0d860643d381711b084f77c73a1ea0ec95c88f5b
Shubham Patel <75959964+jaishiv01@users.noreply.github.com>
2021-01-23 20:32:17 +0530

    Feat : added  Image

--- src/App.js
+++ src/App.js
@@ -1,85 +1,75 @@
 import React from "react";
 import "./styles.css";
 import { useState } from "react";
+import bg from "./netflix.svg";
 
-const colors = "blue";
+const colors = "teal";
 
 const moviesDB = {
-  salman: [
+  Salman: [
     { movie: "Ham aapke hai kon", rating: "IMDB: 8.2" },
-    { movie: "Ham saath saath hai", rating: "IMDB: 8.1" }
+    { movie: "Sultan", rating: "IMDB: 8.1" }
   ],
 
-  TomCruise: [
+  AmirKhan: [
     {
-      movie: "The Mummy",
+      movie: "Dhoom 3",
       rating: "IMDB: 5.4"
     },
     {
-      movie: "Mission: Impossible – Rogue Nation",
+      movie: "Dangal",
       rating: "IMDB: 7.4"
     }
   ],
   TomHolland: [
     {
-      movie: "Spiderman-Far from home",
+      movie: "Spiderman-Far",
       rating: "IMDB: 7.5"
     },
     {
       movie: "The Impossible",
       rating: "IMDB: 7.6"
     }
+  ],
+  HrithikRohsan: [
+    {
+      movie: "Krrish 3",
+      rating: "IMDB: 7.5"
+    },
+    {
+      movie: "Super 30",
+      rating: "IMDB: 7.6"
+    }
   ]
 };
 
 export default function App() {
-  const [selectedActor, setActor] = useState("salman");
+  const [selectedActor, setActor] = useState("Salman");
   function genreClickHandler(name) {
     setActor(name);
   }
   return (
     <div className="App">
-      <h1 className="heading">Movies Recommendation based on Actor Name </h1>
-      <p style={{ fontSize: "smaller" }}>
-        {" "}
-        Checkout my favourite Actor's movies which I like{" "}
-      </p>
+      <div className="container">
+        <img id="wallpaper" src={bg} alt="wallpaper"></img>
+      </div>
+      <div className="container">
+        <h1 className="heading">Movies Recommendation based on Actor Name </h1>
+        <h2> Checkout my favourite Actor's movies </h2>
+      </div>
 
-      <div>
+      <div className="container-fluid">
         {Object.keys(moviesDB).map((name) => (
-          <button
-            onClick={() => genreClickHandler(name)}
-            style={{
-              cursor: "pointer",
-              background: "#16E4D8",
-              borderRadius: "1rem",
-              padding: "1rem  1rem",
-              border: "1px solid black",
-              margin: "1rem 0.3rem"
-            }}
-          >
-            {name}
-          </button>
+          <button onClick={() => genreClickHandler(name)}>{name}</button>
         ))}
       </div>
-      <hr />
-      <div style={{ textAlign: "left" }}>
-        <ul style={{ paddingInlineStart: "0" }}>
+      <div className="container-fluid">
+        <ul>
           {moviesDB[selectedActor].map((name) => (
-            <li
-              key={name.movie}
-              style={{
-                listStyle: "none",
-                padding: "1rem",
-                border: "1px solid #D1D5DB",
-                width: "70%",
-                margin: "1rem 0rem",
-                borderRadius: "0.5rem"
-              }}
-            >
+            <li key={name.movie}>
               {" "}
-              <div style={{ fontSize: "larger" }}> {name.movie} </div>
-              <div style={{ fontSize: "smaller" }}> {name.rating} </div>
+              <div> {name.movie} </div>
+              <div> {name.rating} </div>
             </li>
           ))}
         </ul>
